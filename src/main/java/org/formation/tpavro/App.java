7804678e20c11342a60e2db696888819e6454327
Stéphane <stephane.tiv@gmail.com>
2018-03-08 17:37:58 +0100

    initial commit

---
+++ src/main/java/org/formation/tpavro/App.java
@@ -0,0 +1,13 @@
+package org.formation.tpavro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
